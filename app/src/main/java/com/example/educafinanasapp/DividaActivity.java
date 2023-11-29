86254b0324328890b6f73e01f3c9a0807cdbd96f
Tiago Miranda <t.miranda0249@gmail.com>
2023-11-29 07:58:07 -0300

    Envio do projeto
    Co-Authored-By: LUIZ OLIVEIRA <93673541+Luiz6660@users.noreply.github.com>
    Co-Authored-By: Laura nascente <92733398+itsmelaura07@users.noreply.github.com>
    Co-Authored-By: zigvogel <92961398+zigvogel@users.noreply.github.com>

--- app/src/main/java/com/example/educafinanasapp/DividaActivity.java
+++ app/src/main/java/com/example/educafinanasapp/DividaActivity.java
@@ -1,14 +1,14 @@
 package com.example.educafinanasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class DividaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_divida);
     }
 }
